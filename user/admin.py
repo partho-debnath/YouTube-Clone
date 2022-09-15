a781b36acf4e6e95945ffb355d9f57b438380aa9
Partho Debnath <71305747+pd28CSE@users.noreply.github.com>
2022-09-16 02:14:28 +0600

    Custom User model
    create Custom User model, user creation form, user change form

---
+++ user/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .forms import UserCreationForm, UserChangeForm
+
+from . models import User
+# Register your models here.
+
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'first_name', 'country', 'date_of_birth', 'is_admin')
+    list_filter = ('country', 'is_admin', )
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone', 'date_of_birth',)}),
+        ('Address info', {'fields': ('country', 'city', 'address')}),
+        ('Permissions', {'fields': ('is_admin', 'is_active')}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'date_of_birth', 'country', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', 'phone')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+
+# Now register the new UserAdmin...
+admin.site.register(User, UserAdmin)
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+admin.site.unregister(Group)
+
+#email:admin@gmail.com           # password: django123   
