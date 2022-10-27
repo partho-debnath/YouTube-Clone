6577767b606f51169931d03686fe6223e937842b
Partho Debnath <71305747+pd28CSE@users.noreply.github.com>
2022-10-28 03:45:08 +0600

    UI Update

--- contents/urls.py
+++ contents/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path('', views.VideoContents.as_view(), name='list'),
+    path('', views.VideoContents.as_view(), name='video-list'),
+    path('<str:slug>/', views.SpecificVideoContent.as_view(), name='video-details'),
 ]
--- contents/views.py
+++ contents/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from . models import VideoContent
 
@@ -8,6 +8,18 @@ from . models import VideoContent
 class VideoContents(ListView):
 
     template_name = 'contents/index.html'
-    model = VideoContent
     context_object_name = 'videos'
     ordering = '-uploaded'
+    model = VideoContent
+
+    # def get_queryset(self):
+    #     return VideoContent.objects.all()
+
+
+class SpecificVideoContent(DetailView):
+
+    template_name = 'contents/'
+    context_object_name = 'video'
+    model = VideoContent
+
+     
